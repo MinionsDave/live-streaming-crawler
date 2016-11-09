dcd53fb3af9bc135aef4cf9a592ee42fcbd388c1
MinionsDave <651882883@qq.com>
2016-11-10 00:20:06 +0800

    解决了bug

--- controllers/live.controller.js
+++ controllers/live.controller.js
@@ -4,13 +4,14 @@ const liveCategory = require('../config/liveCategory');
 
 exports.get = function ({ params: { categoryPath = 'lol' }}, res, next) {
     let category = liveCategory[categoryPath];
-    category.path = categoryPath;
     join(crawler.crawlHuya(categoryPath), crawler.crawlPandaTv(categoryPath), crawler.crawlDouyuTv(categoryPath), crawler.crawlZhanqiTv(categoryPath), crawler.crawlQuanminTv(categoryPath), crawler.crawlLongzhu(categoryPath))
         .then(jsonList => {
             let liveJson = [].concat(...jsonList);
             liveJson.sort((o1, o2) => o2.audienceNumber - o1.audienceNumber);
             res.render('index', {
-                category,
+                category: Object.assign({
+                    path: categoryPath
+                }),
                 liveJson: liveJson.slice(0, 150)
             });
         })
@@ -19,13 +20,14 @@ exports.get = function ({ params: { categoryPath = 'lol' }}, res, next) {
 
 exports.getOne = function ({ params: { categoryPath, platform }}, res, next) {
     let category = liveCategory[categoryPath];
-    category.path = categoryPath;
-    category.platform = platform;
     crawler['crawl' + platform[0].toUpperCase() + platform.slice(1)](categoryPath)
         .then(list => {
             list.sort((o1, o2) => o2.audienceNumber - o1.audienceNumber);
             res.render('index', {
-                category,
+                category: Object.assign({
+                    path: categoryPath,
+                    platform
+                }, category),
                 liveJson: list.slice(0, 150)
             })
         })
