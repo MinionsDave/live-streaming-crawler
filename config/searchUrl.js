4e20bfade01a0717eef614b45905fd2e1f27a044
MinionsDave <651882883@qq.com>
2016-11-28 09:09:05 +0800

    同步代码

--- config/searchUrl.js
+++ config/searchUrl.js
@@ -1,3 +1,7 @@
 exports.createPandaSearchUrl = function (keyword) {
     return `http://www.panda.tv/ajax_search?order_cond=fans&pageno=1&name=${ keyword }&pagenum=20`;
 };
+
+exports.createHuyaSearchUrl = function (keyword) {
+    return `http://search.huya.com/?m=Search&do=getSearchContent&q=${keyword}&uid=0&app=11&v=4&typ=-5&rows=40`;
+}
