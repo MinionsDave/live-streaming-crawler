1c7e5d3268b954a87470dab28eb30b3ddf936279
MinionsDave <651882883@qq.com>
2016-12-04 13:44:44 +0800

    加入压力测试代码，不过暂时测试不出什么东西来，后期得还个方案

---
+++ test/stress.test.js
@@ -0,0 +1,28 @@
+const loadtest = require('loadtest');
+const chai = require('chai');
+const request = require('request');
+
+const app = require('../app');
+
+const expect = chai.expect;
+
+chai.config.includeStack = true;
+
+describe('## Stress tests', function() {
+    describe('# homepage should handle 100 requests in a second', function() {
+        it('should ok', function(done) {
+            const options = {
+                url: 'http://localhost:3000',
+                concurrency: 4,
+                maxRequests: 100
+            };
+            loadtest.loadTest(options, function (err, result) {
+                if (err) {
+                    return done(err);
+                }
+                expect(result.totalTimeSeconds).to.be.below(1);
+                done();
+            });
+        });
+    });
+});
