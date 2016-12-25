f77bea919329dce17ef8fe7cd1527cc749c6e8f6
MinionsDave <651882883@qq.com>
2016-12-25 16:01:21 +0800

    修改了测试函数

--- test/crawler.test.js
+++ test/crawler.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 
 const crawler = require('../util/crawler');
-const categoryList = require('../config/liveCategory');
+const categories = require('../config/category');
 
 const expect = chai.expect;
 
@@ -15,15 +15,22 @@ describe('## crawler APIs', () => {
 
 function commonCrawlerTest(crawlFnName, crawlFn) {
     describe(crawlFnName, () => {
-        for(let item of Object.keys(categoryList)) {
+        for(let item of Object.keys(categories)) {
             let example;
-            describe(`get ${item}`, () => {
+            describe(`get ${item}`, function() {
                 it('should return the list and list\'s length large than 0', function(done) {
-                    let res = crawlFn(item);
-                    if (res instanceof Array) {
+
+                    /**
+                     * 如果类目下不存在该平台的url
+                     * 表示该平台没有该直播类目
+                     * 则不做测试
+                     */
+                    if (!categories[item][crawlFnName]) {
                         return this.skip();
                     }
-                    res.then((list) => {
+
+                    let res = crawlFn(item);
+                    res.then(list => {
                         expect(list).to.be.an('array');
                         expect(list.length).to.be.above(1);
                         if (list instanceof Array && list.length > 0) {
