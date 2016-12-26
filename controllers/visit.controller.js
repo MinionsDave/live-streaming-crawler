4dea5287dd243dc1d7fdc63829235bd93c09e322
MinionsDave <651882883@qq.com>
2016-12-26 22:09:05 +0800

    加入了解析ip的代码，并且判断了是否为爬虫

--- controllers/visit.controller.js
+++ controllers/visit.controller.js
@@ -2,10 +2,12 @@
  * @Author: Jax2000
  * @Date: 2016-12-24 16:20:20
  * @Last Modified by: Jax2000
- * @Last Modified time: 2016-12-25 22:15:13
+ * @Last Modified time: 2016-12-26 22:06:43
  */
 const moment = require('moment');
 const winston = require('winston');
+const request = require('superagent');
+const Promise = require('bluebird');
 
 const Visit = require('../models/visit.model');
 const categories = require('../config/category');
@@ -29,23 +31,52 @@ function create(req, res, next) {
         category = '关于本站';
     } else if (!categories[arr[1] || 'lol']) {
 
-        // 类目都不存在，说明404
+        // 类目都不存在，说明404，什么都不用做了
         return;
     } else {
         category = categories[arr[1] || 'lol'].name;
         platform = platforms[arr[2] || 'all'].name;
     }
-    const visit = new Visit({
-        ip: req.headers['x-forwarded-for'] ||
-                req.connection.remoteAddress ||
-                req.socket.remoteAddress,
-        category,
-        platform,
-        visitTime: moment().valueOf(),
-        userAgent: req.headers['user-agent'],
-    });
 
-    visit.save().catch(winston.error);
+    const ip = req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress ||
+                req.socket.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+
+    analysisIp(ip)
+        .then(result => result)
+        .catch(() => ({ip: '错误ip'}))
+        .then(result => {
+            const visit = new Visit(Object.assign({
+                category,
+                platform,
+                visitTime: moment().valueOf(),
+                userAgent,
+                isEngineSpider: userAgent.indexOf('spider') > -1,
+            }, result));
+            visit.save().catch(winston.error);
+        });
+}
+
+/**
+ * 通过淘宝api解析ip
+ * 返回的对象 code不为0就为失败
+ * @param {string} ip - ip地址
+ * @return {Promise.<Object>}
+ */
+function analysisIp(ip) {
+    return new Promise((resolve, reject) => {
+        request
+            .get(`http://ip.taobao.com/service/getIpInfo.php?ip=${ip}`)
+            .then(({text}) => {
+                const data = JSON.parse(text);
+                if (data.code != 0) {
+                    return reject();
+                }
+                resolve(data.data);
+            })
+            .catch(reject);
+    });
 }
 
 module.exports = {create};
--- models/visit.model.js
+++ models/visit.model.js
@@ -2,7 +2,7 @@
  * @Author: Jax2000
  * @Date: 2016-12-24 16:13:43
  * @Last Modified by: Jax2000
- * @Last Modified time: 2016-12-25 20:58:51
+ * @Last Modified time: 2016-12-26 22:04:46
  */
 const mongoose = require('mongoose');
 
@@ -12,6 +12,13 @@ const VisitSchema = new mongoose.Schema({
     platform: String,
     visitTime: Number,
     userAgent: String,
+    isEngineSpider: Boolean, // 是否为搜索引擎爬虫
+    country: String,
+    area: String, // 地区
+    region: String, // 省份
+    city: String,
+    county: String, // 区
+    isp: String,
 });
 
 module.exports = mongoose.model('Visit', VisitSchema);
