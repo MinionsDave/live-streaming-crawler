2da68ec02c2dca85290a2a9184cc20c8baf6148c
MinionsDave <651882883@qq.com>
2016-12-27 19:13:21 +0800

    修改了邮件提醒

--- util/mail.js
+++ util/mail.js
@@ -8,7 +8,11 @@ function sendErrorEmail(err) {
         from: '651882883@qq.com',
         to: 'wudengguang@gmail.com',
         subject: '直播整合平台出现了没有程序处理的异常',
-        html: `<pre>${err}</pre>`,
+        html: `
+            <h1>${err.message}</h1>
+            <h2>${err.status}</h2>
+            <pre>${err.stack}</pre>
+        `,
     };
     transporter.sendMail(mailOptions, function(err) {
         if (err) {
